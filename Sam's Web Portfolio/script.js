a323ddc0673f843e7f9a51cc736b8814ffefebb8
isammittal <152159254+isammittal@users.noreply.github.com>
2024-06-17 18:30:01 +0530

    added all the files
    Hosting my Portfolio

---
+++ Sam's Web Portfolio/script.js
@@ -0,0 +1,18 @@
+document.addEventListener("DOMContentLoaded", () => {
+  document.querySelectorAll("nav ul li a").forEach((anchor) => {
+    anchor.addEventListener("click", function (e) {
+      e.preventDefault();
+
+      if (this.getAttribute("href") === "#") return;
+
+      document.querySelector(this.getAttribute("href")).scrollIntoView({
+        behavior: "smooth",
+      });
+    });
+  });
+
+  document.getElementById("contact").addEventListener("click", function (e) {
+    e.preventDefault();
+    alert("Contact me on: sammittal2010@gmail.com");
+  });
+});
